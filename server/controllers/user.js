744d3c36a351d3c17260df3b2a4b8be6512fd9b8
amayars <amayarajesh24@gmail.com>
2024-10-01 19:41:47 +0530

    first commit

---
+++ server/controllers/user.js
@@ -0,0 +1,97 @@
+import { User } from "../models/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import sendMail from "../middlewares/sendMail.js";
+import TryCatch from "../middlewares/TryCatch.js";
+
+export const register = TryCatch(async(req,res) => {
+    const {email,name,password} = req.body
+
+        let user = await User.findOne({email});
+
+        if(user) 
+            return res.status(400).json({
+        message: "User Already Exists",
+    });
+
+    const hashPassword = await bcrypt.hash(password,10);
+
+    user = {
+        name,
+        email,
+        password: hashPassword
+    }
+
+    const otp = Math.floor(Math.random() *1000000);
+
+    const activationToken = jwt.sign({
+        user,
+        otp,
+    }, process.env.Activation_Secret,
+    {
+        expiresIn: "5m" ,
+    });
+
+    const data = {
+        name, otp
+    };
+
+    await sendMail(
+        email,
+        "E Learning",
+        data
+    );
+
+    res.status(200).json({
+        message: "Otp send to your mail" ,
+        activationToken,
+    });
+})
+
+export const verifyUser = TryCatch(async(req,rest) =>{
+    const {otp, activationToken} = req.body
+
+    const verify = jwt.verify(activationToken, process.env.Activation_Secret)
+
+    if (!verify)
+        return res.status(400).json({
+            message: "Otp Expired",
+    });
+
+    if (verify.otp !== otp)return res.status(400).json({
+        message: "Wrong Otp",
+    });
+
+    await User.create ({
+        name: verify.user.name,
+        email: verify.user.email,
+        password: verify.user.password,
+    });
+
+    res.json({
+        message: "User Registered",
+    });
+});
+
+export const loginUser = TryCatch(async(req,res) => {
+    const {email, password} = req.body
+    const user = await User.findOne({email})
+
+    if(!user) return res.status(400).json({
+        message: "No User with this email",
+    });
+    const matchPassword = await bcrypt.compare(password, user.password);
+
+    if(!matchPassword)
+        return res.status(400).json ({
+    message: "wrong password",
+});
+    const token = await jwt.sign({_id: user._id}, process.env.Jwt_Sec,{
+        expiresIn: "15d",
+    });
+    res.json({
+        message: `Welcome back ${user.name}` ,
+        token ,
+        user,
+    });
+});
