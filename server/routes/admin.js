a45b9eefaed2c3a345a719cb5425c361da378593
Guru Varsheni <guruvarsheni@gmail.com>
2024-10-04 20:55:39 +0530

    changes

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import { isAuth, isAdmin } from '../middlewares/isAuth.js';
-import { addLectures, createCourse, deleteLecture } from '../controllers/admin.js';
+import { addLectures, createCourse, deleteCourse, deleteLecture } from '../controllers/admin.js';
 import { uploadFiles } from '../middlewares/multer.js';
 
 const router = express.Router();
 
 router.post('/course/new', isAuth, isAdmin, uploadFiles, createCourse);
 router.post("/course/:id" , isAuth, isAdmin, uploadFiles, addLectures);
+router.delete("/course/:id" , isAuth, isAdmin, deleteCourse);
 router.delete("/lecture/:id", isAuth, isAdmin, deleteLecture);
 
 export default router;
