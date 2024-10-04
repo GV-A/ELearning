a57a3a74bf6f9ae69eab8d141b252e6ea55751f4
Guru Varsheni <guruvarsheni@gmail.com>
2024-10-05 04:35:44 +0530

    changes

--- server/routes/course.js
+++ server/routes/course.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllCourses, getSingleCourse, fetchLectures, fetchLecture, getMyCourses } from "../controllers/course.js";
+import { getAllCourses, getSingleCourse, fetchLectures, fetchLecture, getMyCourses, checkout, paymentVerification } from "../controllers/course.js";
 import { isAuth} from "../middlewares/isAuth.js";
 
 const router = express.Router();
@@ -9,5 +9,7 @@ router.get("/course/:id", getSingleCourse);
 router.get("/lectures/:id" , isAuth, fetchLectures);
 router.get("/lecture/:id" , isAuth, fetchLecture);
 router.get("/mycourse", isAuth, getMyCourses);
+router.post("/course/checkout/:id", isAuth, checkout);
+router.post("/verification/:id", isAuth, paymentVerification);
 
 export default router;
